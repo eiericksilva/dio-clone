d305afb4623d4e9f10614bdfd3dbbca2696fc608
Erick <ericksilvaredes@gmail.com>
2022-12-13 11:41:53 -0300

    finalizando tela home

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,15 +1,31 @@
-import { Link } from 'react-router-dom'
+
 import Button from '../../components/button'
 import React from 'react'
 import Header from '../../components/header'
+import { Container, TextContent, Title, TitleHighlight } from './styles'
+import bannerImage from '../../assets/banner.png'
 
 const Home = () => {
   return (
     <>
       <Header />
-      <Button title='teste1'></Button>
-      <Button title='teste1' variant='secondary' />
-      <Link to='/login'>Go Login</Link>
+      <Container>
+        <div>
+          <Title> 
+            <TitleHighlight>
+              Implemente<br /> 
+            </TitleHighlight>
+              o o seu futuro global agora!
+          </Title>
+          <TextContent>
+            Domine as tecnologias utilizadas pelas empresas mais inovadoras do mundo e encare seu novo desafio profissional, evoluindo em comunidade com os melhores experts.
+          </TextContent>
+          <Button title="Começar agora" variant="secondary" />
+        </div>
+        <div>
+          <img src={bannerImage} alt="Imagem Principal" />
+        </div>
+      </Container>
     </>
    
   )
