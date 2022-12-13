742896c16350de8ba0383cb8be5065510feaff45
Erick <ericksilvaredes@gmail.com>
2022-12-13 11:14:42 -0300

    Estilo global

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Login from "./pages/login";
+import { GlobalStyle } from "./global";
+
 function App() {
 
   return (
     <BrowserRouter>
+      <GlobalStyle />
       <Routes>
         <Route path="/" element={<Home/>}></Route>
         <Route path="/login" element={<Login/>}></Route>
