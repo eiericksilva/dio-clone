79e2f3739d83fbf49172951dcd54b2d7e211cd5e
Erick <ericksilvaredes@gmail.com>
2022-12-13 09:57:13 -0300

    fist commit

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+        <div>Home</div>
+        <Link to='/login'>Go Login</Link>
+    </>
+   
+  )
+}
+
+export default Home
